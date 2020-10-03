a4850ec4ad09806e45af811d47deda5ad9aeb8de
Michaek Luk <52308484+LuniumLuk@users.noreply.github.com>
2020-10-03 22:57:55 +0800

    Update 2020/10/3

--- hello.py
+++ hello.py
@@ -4,24 +4,29 @@ import sys
 
 vuefile = open("firstvue.html", "r")
 logfile = open("gunicorn.log", "r")
+musicfile = open("music.html", "r")
 app = Flask(__name__)
 
 vuecontent = vuefile.read()
 logcontent = logfile.read()
+musiccontent = musicfile.read()
 
 vuefile.close()
 logfile.close()
+musicfile.close()
 
 def get_music_text_content(root):
-	file = os.listdir(root)
-	music_text = ''
+	path = sys.path[0]
+	file = os.listdir(path + '/' + root)
+	music_text = musiccontent
 	for f in file:
 		if f.endswith('.txt'):
-			f_content = open(root+'/'+f)
-			music_text = music_text + "<p>" + f_content.read() + "</p>"
-			music_text = music_text + "<img src = '" + root + "/" + f[:-3] + "png'/>"
+			f_content = open(path + '/' + root+'/'+f)
+			music_text = music_text + "\n<div class = 'music-card'>\n<p style = 'width: 40%'>" + f_content.read() + "</p>"
+			music_text = music_text + "\n<img class = 'music-cover' height = 120px width = 120px src = '" + root + '/' + f[:-3] + "png' alt = '" + f[:-3] + "png' />"
+			music_text = music_text + "\n<audio class = 'music-player' src = '" + root + '/' + f[:-3] + "mp3' controls = 'controls'>Your Browser does not support audio</audio>\n</div>"
 			f_content.close()
-	return music_text
+	return music_text + "\n</body>\n</html>"
 
 @app.route('/')
 def hello_world():
@@ -33,11 +38,11 @@ def log():
 
 @app.route('/music_01')
 def music_01():
-	return get_music_text_content('/data/music_01')
+	return get_music_text_content('static/data/music_01')
 
 @app.route('/music_02')
 def music_02():
-	return get_music_text_content('/data/music_02')
+	return get_music_text_content('static/data/music_02')
 
 
 
